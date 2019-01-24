381a125610a76d2d29093755d38cdd01e43f6ca2
Zeritfreeman <47009881+Zeritfreeman@users.noreply.github.com>
2019-01-24 15:33:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite la longitud de la hipotenusa ");
+            int t = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite el angulo a ");
+            int a = int.Parse(Console.ReadLine());
+
+            double c = 90.0 - a;  
+            double y = Math.Cos(a * (Math.PI / 180.0)) * t;
+            double z = Math.Sin(a * (Math.PI / 180.0)) * t;
+
+            Console.WriteLine("El angulo c es " + c);
+            Console.WriteLine("El lado y es " + y);
+            Console.WriteLine("El lado z es " + z);
+        }
+    }
+}
