475addec5e2bb9ae31a729235b496629d699f88c
Zeritfreeman <47009881+Zeritfreeman@users.noreply.github.com>
2019-01-24 15:46:48 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite la longitud del lado z ");
+            double z = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite el angulo c ");
+            double c = double.Parse(Console.ReadLine());
+
+            double a = 90.0 - c;
+            double t = z / Math.Sin(c * (Math.PI / 180.0));
+            double y = Math.Sqrt((t * t) - (z * z));
+
+            Console.WriteLine("El angulo a es " + a);
+            Console.WriteLine("La hipotenusa es " + t);
+            Console.WriteLine("El lado y es " + y);
+        }
+    }
+}
